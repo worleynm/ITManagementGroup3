2789379e54568b364a83921c8b3d09b25d4c4e43
Morri4md <Morri4md@mail.uc.edu>
2024-10-22 13:50:18 -0400

    Update app.py
    add navigation

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/home")
-def hello():
-    return "Hello, World!"
+# Homepage route
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-@app.route("/project_charter")
-def hello():
-    return "Hello, World!"
+# About page route
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-@app.route("/team_profile")
-def hello():
-    return "Hello, World!"
+# Services page route
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+# Contact page route
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+# Projects page route
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+# Blog page route
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+# NEED TO CHANGE FILE NAMES STILL
