c00c8be8550d25db8b22b0bd47e99a63aba0a26e
Ian Listopad <ian@ians-mbp-3.wifi.uc.edu>
2024-10-22 13:32:50 -0400

    add in html pages

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
+def hello():
+    return "Hello, World!"
+
+@app.route("/project_charter")
+def hello():
+    return "Hello, World!"
+
+@app.route("/team_profile")
 def hello():
     return "Hello, World!"
